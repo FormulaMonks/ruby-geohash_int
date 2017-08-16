be36a799bcc75f0ed17bb26bf9cb10f64f05489d
Ary Borenszweig <asterite@gmail.com>
2017-08-16 12:03:41 -0300

    Automatically generate shared library when running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,17 @@
 require "bundler/setup"
+
+require "ffi"
+
+# Automatically generate the shared library if it doesn't exist
+lib_filename = File.expand_path("../../ext/geohash_int/geohash.#{::FFI::Platform::LIBSUFFIX}", __FILE__)
+unless File.exist?(lib_filename)
+  Dir.chdir(File.expand_path("../../ext/geohash_int", __FILE__)) do
+    unless system("rake")
+      fail "couldn't generate shared library"
+    end
+  end
+end
+
 require "geohash_int"
 
 RSpec.configure do |config|
